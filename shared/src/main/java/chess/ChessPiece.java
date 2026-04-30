0f569bc8ccd79e483bf3c1748281b5503c8fe792
leesyd01 <leesyd01@byu.edu>
2026-04-30 17:46:45 -0600

    rook implemented successfully

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -98,9 +98,32 @@ public class ChessPiece {
                     null));
         }
         if (piece.getPieceType() == PieceType.ROOK) {
-            return List.of(new ChessMove(myPosition, new ChessPosition(myPosition.getRow(), myPosition.getColumn()),
-                    null));
+            ArrayList<ChessMove> moves = new ArrayList<>();
+            int[][] directions = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+
+            for (int[] dir : directions) {
+                int r = myPosition.getRow() + dir[0];
+                int c = myPosition.getColumn() + dir[1];
+
+                while (r >=1 && r <= 8 && c >= 1 && c <= 8) {
+                    ChessPosition endPos = new ChessPosition(r, c);
+                    ChessPiece occupant = board.getPiece(endPos);
+
+                    if (occupant == null) {
+                        moves.add(new ChessMove(myPosition, endPos, null));
+                    } else if (occupant.getTeamColor() != piece.getTeamColor()) {
+                        moves.add(new ChessMove(myPosition, endPos, null));
+                        break;
+                    } else {
+                        break;
+                    }
+                    r += dir[0];
+                    c += dir[1];
+                }
+            }
+            return moves;
         }
+
         if (piece.getPieceType() == PieceType.PAWN) {
             return List.of(new ChessMove(myPosition, new ChessPosition(myPosition.getRow(), myPosition.getColumn()),
                     null));
