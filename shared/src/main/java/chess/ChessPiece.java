4d12c214f843576ec59d5b81e9dddecefa46f481
leesyd01 <leesyd01@byu.edu>
2026-04-30 19:01:19 -0600

    queen implemented successfully

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -59,7 +59,7 @@ public class ChessPiece {
         ChessPiece piece = board.getPiece(myPosition);
         if (piece.getPieceType() == PieceType.KING) {
             ArrayList<ChessMove> moves = new ArrayList<>();
-            int[][] directions = {{0, 1}, {0, -1}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}, {1, 0}, {-1, 0}}; // put in moves
+            int[][] directions = {{0, 1}, {0, -1}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}, {1, 0}, {-1, 0}};
 
             for (int[] dir : directions) {
                 int r = myPosition.getRow() + dir[0];
@@ -73,9 +73,6 @@ public class ChessPiece {
                         moves.add(new ChessMove(myPosition, endPos, null));
                     } else if (occupant.getTeamColor() != piece.getTeamColor()) {
                         moves.add(new ChessMove(myPosition, endPos, null));
-//                        break;
-//                    } else {
-//                        break;
                     }
                     r += dir[0];
                     c += dir[1];
@@ -85,8 +82,30 @@ public class ChessPiece {
         }
 
         if (piece.getPieceType() == PieceType.QUEEN) {
-            return List.of(new ChessMove(myPosition, new ChessPosition(myPosition.getRow(), myPosition.getColumn()),
-                    null));
+            ArrayList<ChessMove> moves = new ArrayList<>();
+            int[][] directions = {{0, 1}, {0, -1}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}, {1, 0}, {-1, 0}}; // put in moves
+
+            for (int[] dir : directions) {
+                int r = myPosition.getRow() + dir[0];
+                int c = myPosition.getColumn() + dir[1];
+
+                while (r >= 1 && r <= 8 && c >= 1 && c <= 8) {
+                    ChessPosition endPos = new ChessPosition(r, c);
+                    ChessPiece occupant = board.getPiece(endPos);
+
+                    if (occupant == null) {
+                        moves.add(new ChessMove(myPosition, endPos, null));
+                    } else if (occupant.getTeamColor() != piece.getTeamColor()) {
+                        moves.add(new ChessMove(myPosition, endPos, null));
+                        break;
+                    } else {
+                        break;
+                    }
+                    r += dir[0];
+                    c += dir[1];
+                }
+            }
+            return moves;
         }
 
         if (piece.getPieceType() == PieceType.BISHOP) {
