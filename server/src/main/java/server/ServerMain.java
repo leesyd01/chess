64451f6199d57b51499071ddec2685caf042e677
leesyd01 <leesyd01@byu.edu>
2026-05-21 02:02:25 -0600

    bug fixes

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -38,9 +38,4 @@ public class ServerMain {
 
         return app.port();
     }
-
-    public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
-    }
 }
