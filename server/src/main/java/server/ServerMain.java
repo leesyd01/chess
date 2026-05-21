af0c3ac0849ec615b8578aeac6b816b11ca95681
leesyd01 <leesyd01@byu.edu>
2026-05-20 20:32:03 -0600

    added handlers

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -2,12 +2,23 @@ package server;
 
 import chess.*;
 import io.javalin.Javalin;
+import dataAccess.DataAccess;
 
 public class ServerMain {
     private Javalin app;
 
     public static void main(String[] args) {
         new ServerMain().run(8080);
+    }
+
+    public int run(int desiredPort) {
+        DataAccess dataAccess = new MemoryDataAccess();
+        UserHandler userHandler = new UserHandler(new UserService(dataAccess));
+        GameHandler gameHandler = new GameHandler(new GameService(dataAccess));
+        ClearHandler clearHandler = new ClearHandler(new ClearService(dataAccess));
+
+
+
     }
 
     public static void main(String[] args) {
