ff1b1c116248a95f2f982aaba35f05b9bce224db
leesyd01 <leesyd01@byu.edu>
2026-05-20 20:27:14 -0600

    added server port

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -1,8 +1,15 @@
 package server;
 
 import chess.*;
+import io.javalin.Javalin;
 
 public class ServerMain {
+    private Javalin app;
+
+    public static void main(String[] args) {
+        new ServerMain().run(8080);
+    }
+
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
