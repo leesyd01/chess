971431f3815681f89482207ef728c162b163cd62
leesyd01 <leesyd01@byu.edu>
2026-05-21 00:11:55 -0600

    added handler for clearing data

---
+++ server/src/main/java/handler/ClearHandler.java
@@ -0,0 +1,22 @@
+package handler;
+
+import io.javalin.http.Context;
+import service.ClearService;
+import service.ServiceException;
+
+public class ClearHandler {
+    private final ClearService clearService;
+
+    public ClearHandler(ClearService clearService) {
+        this.clearService = clearService;
+    }
+
+    public void clear(Context cxt) {
+        try {
+            clearService.clear();
+            ctx.status(200).json("{}");
+        } catch (ServiceException e) {
+            ctx.status(e.statusCode()).json("{\"message\": \"Error: " + e.getMessage() + "\"}");
+        }
+    }
+}
