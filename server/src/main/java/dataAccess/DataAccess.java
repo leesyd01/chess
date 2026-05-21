0ed5420fb475daa159518c87f6a840b3ca6d511a
leesyd01 <leesyd01@byu.edu>
2026-05-20 23:21:03 -0600

    data access implemented

--- server/src/main/java/dataAccess/DataAccess.java
+++ server/src/main/java/dataAccess/DataAccess.java
@@ -1,1 +1,28 @@
 // interface
+package dataAccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+
+public interface DataAccess {
+    // user
+    void createUser(UserData userData) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+
+    // auth
+    AuthData createAuth(String username) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+
+    // game
+    GameData createGame(String gameName) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    Collection<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData gameData) throws DataAccessException;
+
+    // util
+    void clear() throws DataAccessException;
+}
