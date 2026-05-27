0cec8f80dc418b5872239ebb11f52720a1600e4a
leesyd01 <leesyd01@byu.edu>
2026-05-26 22:40:30 -0600

    fixed bug

--- server/src/main/java/dataaccess/DataAccessException.java
+++ server/src/main/java/dataaccess/DataAccessException.java
@@ -4,4 +4,7 @@ public class DataAccessException extends Exception {
     public DataAccessException(String message) {
         super(message);
     }
+    public DataAccessException(String message, Exception cause) {
+        super(message, cause);
+    }
 }
