f0512081f14b7391e6c53a7a26c1ea2bbfc600e5
leesyd01 <leesyd01@byu.edu>
2026-06-02 17:41:53 -0600

    updated client main

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,37 @@
 package client;
 
 import chess.*;
+import model.AuthData;
+import ui.PostloginUI;
+import ui.PreloginUI;
+import java.util.Scanner;
 
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        int port = 8080;
+        if (args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException ignored) {}
+        }
+
+        ServerFacade facade = new ServerFacade(port);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            PreloginUI prelogin = new PreloginUI(facade, scanner);
+            AuthData auth = prelogin.run();
+
+            if (auth == null) {
+                break;
+            }
+
+            PostloginUI postlogin = new PostloginUI(facade, scanner, auth);
+            boolean loggedOut = postlogin.run();
+
+            if (!loggedOut) {
+                break;
+            }
+        }
     }
 }
