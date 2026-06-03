f83b3a6f8aee08e905b381073cd1dbf58e8f37bc
leesyd01 <leesyd01@byu.edu>
2026-06-02 19:16:14 -0600

    more bug fixes

--- client/src/main/java/ui/BoardDrawer.java
+++ client/src/main/java/ui/BoardDrawer.java
@@ -37,8 +37,8 @@ public class BoardDrawer {
                 : new int[]{0, 1, 2, 3, 4, 5, 6, 7};
 
         int[] rowOrder = flipped
-                ? new int[]{7, 6, 5, 4, 3, 2, 1, 0}
-                : new int[]{0, 1, 2, 3, 4, 5, 6, 7};
+                ? new int[]{8, 7, 6, 5, 4, 3, 2, 1}
+                : new int[]{1, 2, 3, 4, 5, 6, 7, 8};
 
         printColumnHeaders(cols, colOrder);
         for (int row : rowOrder) {
