835050eef2fe4dcd02a415021ceb928301bcb9ad
leesyd01 <leesyd01@byu.edu>
2026-06-11 01:43:45 -0600

    added ws server endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -27,7 +27,17 @@ public class Server {
         GameHandler gameHandler   = new GameHandler(new GameService(dataAccess));
         ClearHandler clearHandler = new ClearHandler(new ClearService(dataAccess));
 
+        WebSocketHandler wsHandler = new WebSocketHandler(gameService, userService);
+
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
+
+        // WebSocket endpoint
+        javalin.ws("/ws", ws -> {
+            ws.onConnect(ctx -> {});
+            es.onMessage(ctx -> wsHandler.onMessage(ctx.session, ctx.message()));
+            ws.onClose(ctx -> wsHandler.onClose(ctx.session, ctx.status(), ctx.reason()));
+            es.onError(ctx -> wsHandler.onError(ctx.session, ctx.error()));
+        });
 
         javalin.post("/user",      userHandler::register);
         javalin.post("/session",   userHandler::login);
